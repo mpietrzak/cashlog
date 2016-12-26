6c55d1154cb901c620d13dcf6f31a1551346d721
Maciej Pietrzak <mpietrzak@gmail.com>
2016-12-26 20:45:22 +0100

    Default entry time.

--- src/tmpl/add.rs
+++ src/tmpl/add.rs
@@ -1,5 +1,6 @@
 
 use maud::Markup;
+use time;
 
 use tmpl::common;
 use std::collections::BTreeMap;
@@ -13,11 +14,11 @@ fn geth(oh: &HashMap<&str, String>, key: &str) -> String {
     }
 }
 
-fn getb(ob: &BTreeMap<String, String>, key: &str) -> String {
+fn getb(ob: &BTreeMap<String, String>, key: &str, default: &str) -> String {
     let ov = ob.get(key);
     match ov {
         Some(v) => v.clone(),
-        None => String::from("")
+        None => default.to_string()
     }
 }
 
@@ -25,11 +26,13 @@ fn simple_field(
     values: &BTreeMap<String, String>,
     errors: &HashMap<&str, String>,
     title: &str,
-    key: &str) -> Markup {
+    key: &str,
+    default: &str
+    ) -> Markup {
     html! {
         tr {
             td (title)
-            td input type="text" name=(key) value=(getb(values, key))
+            td input type="text" name=(key) value=(getb(values, key, default))
             td class="error" (geth(errors, key))
         }
     }
@@ -39,14 +42,15 @@ pub fn tmpl_add(
         title: &str,
         values: &BTreeMap<String, String>,
         errors: &HashMap<&str, String>) -> Markup {
+    let now = time::strftime("%Y-%m-%d %H:%M:%S", &time::now_utc()).unwrap();
     let form = html! {
         form method="post" {
             table class="form" {
                 tbody {
-                    (simple_field(values, errors, "Account:", "account"))
-                    (simple_field(values, errors, "Date/Time:", "ts"))
-                    (simple_field(values, errors, "Amount:", "amount"))
-                    (simple_field(values, errors, "Currency:", "currency"))
+                    (simple_field(values, errors, "Account:", "account", ""))
+                    (simple_field(values, errors, "Date/Time:", "ts", &now))
+                    (simple_field(values, errors, "Amount:", "amount", ""))
+                    (simple_field(values, errors, "Currency:", "currency", ""))
                     tr {
                         td colspan="3" align="right" {
                             button type="submit" "Ok"
